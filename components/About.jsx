80407c52797edbe0fd0a506761bd5e98919bdf6b
ChinonsoIg <ojononso@yahoo.com>
2023-01-10 16:07:39 +0100

    build about section

---
+++ components/About.jsx
@@ -0,0 +1,44 @@
+import Image from "next/image";
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="w-full md:h-screen p-2 flex items-center py-16">
+      <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
+        <div className="col-span-2">
+          <p className="uppercase text-xl tracking-widest text-[#5651e5]">About</p>
+          <p className="py-4">Who I Am</p>
+          <p className="py-2 text-gray-400">// Not your normal developer</p>
+          <p className="py-2 text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry"s standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </p>
+          <p className="py-2 text-gray-600">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry. Lorem Ipsum has been the industry"s standard dummy text
+            ever since the 1500s, when an unknown printer took a galley of type
+            and scrambled it to make a type specimen book. It has survived not
+            only five centuries, but also the leap into electronic typesetting,
+            remaining essentially unchanged. It was popularised in the 1960s
+            with the release of Letraset sheets containing Lorem Ipsum passages,
+            and more recently with desktop publishing software like Aldus
+            PageMaker including versions of Lorem Ipsum
+          </p>
+          <p className="py-2 text-gray-600 underline cursor-pointer">Check out some of my latest projetcs</p>
+        </div>
+        <div className="w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex justify-center items-center p-4 hover:scale-105 ease-in duration-300">
+          <Image src="/../public/assets/me/pic.jpeg" width="277" height="363" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,8 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import React, {useState } from 'react';
-import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import { navItems, socials } from '../utils/Functions';
+import Image from "next/image";
+import Link from "next/link";
+import React, {useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+import { navItems, socials } from "../utils/Functions";
 
 
 const Navbar = () => {
@@ -13,55 +13,55 @@ const Navbar = () => {
   }
 
   return (
-    <div className='fixed w-full h-20 shadow-xl z-[100]'>
-      <div className='flex justify-between items-center w-full h-full px-2 2xl-px-16'>
-        <Image src='/../public/assets/skills/firebase.png' width='50' height='20' />
+    <div className="fixed w-full h-20 shadow-xl z-[100] bg-white">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl-px-16">
+        <Image src="/../public/assets/skills/firebase.png" width="50" height="20" />
         <div>
-          <ul className='hidden md:flex'>
+          <ul className="hidden md:flex">
             {
               navItems.map((navItem) => (
                 <Link href={navItem.url} key={navItem.id}>
-                  <li className='ml-10 text-sm uppercase hover:border-b'>{navItem.name}</li>
+                  <li className="ml-10 text-sm uppercase hover:border-b">{navItem.name}</li>
                 </Link>
               ))
             }
           </ul>
-          <div onClick={handleNav} className='md:hidden'>
+          <div onClick={handleNav} className="md:hidden">
             <AiOutlineMenu size={25} />
           </div>
         </div>
       </div>
 
-      <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
-        <div className={nav ? 'md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-5 ease-in duration-500' : 'fixed left-[-120%] top-0 p-5 ease-in duration-500'}>
+      <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
+        <div className={nav ? "md:hidden fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-5 ease-in duration-500" : "fixed left-[-120%] top-0 p-5 ease-in duration-500"}>
           <div>
-            <div className='flex justify-between items-center w-full'>
-              <Image src='/../public/assets/skills/firebase.png' width='50' height='20' />
-              <div onClick={handleNav} className='rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer'>
+            <div className="flex justify-between items-center w-full">
+              <Image src="/../public/assets/skills/firebase.png" width="50" height="20" />
+              <div onClick={handleNav} className="rounded-full shadow-lg shadow-gray-400 p-2 cursor-pointer">
                 <AiOutlineClose size={20} />
               </div>
             </div>
-            <div className='border-b border-gray-300 my-4'>
-              <p className='w-[85%] md:w-[90%] py-4 text-base'>Let&apos;s build something legendary together.</p>
+            <div className="border-b border-gray-300 my-4">
+              <p className="w-[85%] md:w-[90%] py-4 text-base">Let&apos;s build something legendary together.</p>
             </div>
           </div>
 
-          <div className='py-4 flex flex-col text-left'>
+          <div className="py-4 flex flex-col text-left">
             <ul >
               {
                 navItems.map((navItem) => (
                   <Link href={navItem.url} key={navItem.id}>
-                    <li className='text-sm uppercase py-3'>{navItem.name}</li>
+                    <li className="text-sm uppercase py-3">{navItem.name}</li>
                   </Link>
                 ))
               }
             </ul>
-            <div className='pt-40'>
-              <p className='uppercase tracking-widest text-[#5651e5]'>Let&apos;s connect</p>
-              <div className='flex justify-between items-center my-4 w-full sm:w-[80%]'>
+            <div className="pt-40">
+              <p className="uppercase tracking-widest text-[#5651e5]">Let&apos;s connect</p>
+              <div className="flex justify-between items-center my-4 w-full sm:w-[80%]">
                 {
                   socials.map((social) => (
-                    <div className='rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300' key={social.id}>
+                    <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in duration-300" key={social.id}>
                       <Link href={social.url} target="_blank">{social.icon}</Link>
                     </div>
                   ))
