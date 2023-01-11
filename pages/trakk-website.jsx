ad5d45851ffff814214ef2def43fdecb1439479f
ChinonsoIg <ojononso@yahoo.com>
2023-01-11 15:40:21 +0100

    design trakk website

--- pages/trakk-website.jsx
+++ pages/trakk-website.jsx
@@ -2,22 +2,22 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { RiRadioButtonFill } from "react-icons/ri";
-// import { FiExternalLink } from "react-icons/fi";
-import merchant_dashboard from "../public/assets/projects/merchant_dashboard.png";
+import { FiExternalLink } from "react-icons/fi";
+import trakk_website from "../public/assets/projects/trakk_website.png";
 
-const MerchantDashboard = () => {
+const TrakkWebsite = () => {
   return (
     <div className="w-full">
       <div className="w-screen h-[30vh] lg:h-[40vh] relative">
         <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
         <Image
-          src={merchant_dashboard}
+          src={trakk_website}
           alt="Sales and inventory management"
           className="absolute z-1 object-cover"
           fill
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Merchant Dashboard</h2>
+          <h2 className="py-2">Trakk Website</h2>
           <h3>Next Js / Chakra UI / TypeScript</h3>
         </div>
       </div>
@@ -47,10 +47,10 @@ const MerchantDashboard = () => {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum
           </p>
-          {/* <div className="flex justify-start items-center gap-3">
+          <div className="flex justify-start items-center gap-3">
             <button className="px-8 py-2 mt-4 mr-8">
               <Link
-                href="https://chinonso-myreads-app.netlify.app"
+                href="https://trakkhq.com"
                 target="_blank"
                 className="flex justify-between items-center gap-3"
               >
@@ -58,7 +58,7 @@ const MerchantDashboard = () => {
                 <FiExternalLink />
               </Link>
             </button>
-            <button className="px-8 py-2 mt-4">
+            {/* <button className="px-8 py-2 mt-4">
               <Link
                 href="https://github.com/ChinonsoIg/myreads"
                 target="_blank"
@@ -67,8 +67,8 @@ const MerchantDashboard = () => {
                 Code
                 <FiExternalLink />
               </Link>
-            </button>
-          </div> */}
+            </button> */}
+          </div>
         </div>
 
         <div className="col-span-4 md:col-span-2 lg:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
@@ -89,11 +89,7 @@ const MerchantDashboard = () => {
               </p>
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill size={16} className="pr-1" />
-                Context API
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                Graphh QL
+                Tidio
               </p>
             </div>
           </div>
@@ -106,4 +102,4 @@ const MerchantDashboard = () => {
   );
 };
 
-export default MerchantDashboard;
+export default TrakkWebsite;
