adbea6d12d9eb7abfe17f135cc208155bd47c54d
ChinonsoIg <ojononso@yahoo.com>
2023-01-11 16:07:04 +0100

    design the photo grid page

--- pages/photo-grid.jsx
+++ pages/photo-grid.jsx
@@ -2,22 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { RiRadioButtonFill } from "react-icons/ri";
-import sales_and_inventory_management from "../public/assets/projects/sales_and_inventory_management.png";
+import { FiExternalLink } from "react-icons/fi";
+import photo_grid from "../public/assets/projects/photo_grid.png";
 
-const SalesInventory = () => {
+const PhotoGrid = () => {
   return (
     <div className="w-full">
       <div className="w-screen h-[30vh] lg:h-[40vh] relative">
         <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
         <Image
-          src={sales_and_inventory_management}
+          src={photo_grid}
           alt="Sales and inventory management"
           className="absolute z-1 object-cover"
           fill
         />
         <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2">Sales & Inventory Management System</h2>
-          <h3>Next Js / Chakra UI / Node Js</h3>
+          <h2 className="py-2">Photo Grid App</h2>
+          <h3>Vue Js</h3>
         </div>
       </div>
 
@@ -46,8 +47,28 @@ const SalesInventory = () => {
             and more recently with desktop publishing software like Aldus
             PageMaker including versions of Lorem Ipsum
           </p>
-          {/* <button className="px-8 py-2 mt-4 mr-8">Demo</button>
-          <button className="px-8 py-2 mt-4">Code</button> */}
+          <div className="flex justify-start items-center gap-3">
+            <button className="px-8 py-2 mt-4 mr-8">
+              <Link
+                href="https://unsplash-photo-grid.netlify.app"
+                target="_blank"
+                className="flex justify-between items-center gap-3"
+              >
+                Demo
+                <FiExternalLink />
+              </Link>
+            </button>
+            <button className="px-8 py-2 mt-4">
+              <Link
+                href="https://github.com/ChinonsoIg/photo-grid"
+                target="_blank"
+                className="flex justify-between items-center gap-3"
+              >
+                Code
+                <FiExternalLink />
+              </Link>
+            </button>
+          </div>
         </div>
 
         <div className="col-span-4 md:col-span-2 lg:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
@@ -56,23 +77,7 @@ const SalesInventory = () => {
             <div className="grid grid-col-3 md:grid-cols-1">
               <p className="text-gray-600 py-2 flex items-center">
                 <RiRadioButtonFill size={16} className="pr-1" />
-                Next Js
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                Context API
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                Chakra UI
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                Node Js
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                MySQL
+                Vue Js
               </p>
             </div>
           </div>
@@ -81,10 +86,9 @@ const SalesInventory = () => {
         <p className="underline cursor-pointer py-10 w-max">
           <Link href="/#projects">Back</Link>
         </p>
-
       </div>
     </div>
   );
 };
 
-export default SalesInventory;
+export default PhotoGrid;
