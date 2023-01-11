7d205c365a0a0884a9df3d42eececa970c43a9bb
ChinonsoIg <ojononso@yahoo.com>
2023-01-11 09:13:03 +0100

    include technology used

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -22,7 +22,7 @@ const Projects = () => {
             />
             <div className="hidden group-hover:block absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]">
               <h3 className="text-2xl text-white tracking-wider text-center">{project.name}</h3>
-              <p className="pb-4 pt-2 text-white text-center">React Js</p>
+              <p className="pb-4 pt-2 text-white text-center">{project.technology}</p>
               <Link href="/">
                 <p className="text-center py-3 rounded-lg bg-white text-gray-700 font-bold text-lg cursor-pointer">More Info</p>
               </Link>
