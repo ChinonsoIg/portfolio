946518faa9e096c80079a58547f404383dc4a719
ChinonsoIg <ojononso@yahoo.com>
2023-01-25 02:57:27 +0100

    convert would-you-rather page to use the SingleProject component

--- pages/would-you-rather.jsx
+++ pages/would-you-rather.jsx
@@ -4,81 +4,38 @@ import React from "react";
 import { RiRadioButtonFill } from "react-icons/ri";
 import { FiExternalLink } from "react-icons/fi";
 import would_you_rather_game from "../public/assets/projects/would_you_rather_game.png";
+import SingleProject from "../components/SingleProject";
 
 const WouldYouRather = () => {
+
+  const details =
+    <>
+      This is a version of the would you rather game. Users can ask questions and answer any question. For each question asked or answered, a score of 1 is given to the user. To see the score of all the users, you can click on the Leaderboard. <br />
+      This was built with react and state management with redux toolkit. Through this app, I gained useful experience on redux State management.
+    </>
+
+  const stacks = [
+    { id: "1", name: "Next Js" },
+    { id: "2", name: "Redux Toolkit" },
+    { id: "3", name: "Semantic UI" },
+  ];
+
+  const technologies = [
+    { id: "1", name: "Next Js" },
+    { id: "2", name: "Redux Toolkit" },
+    { id: "3", name: "Semantic UI" },
+  ];
+
   return (
-    <div className="w-full">
-      <div className="w-full h-[30vh] lg:h-[40vh] relative">
-        <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
-        <Image
-          src={would_you_rather_game}
-          alt="Sales and inventory management"
-          className="absolute z-1 object-cover"
-          fill
-        />
-        <div className="absolute top-[70%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] translate-y-[-50%] text-white z-10 p-2">
-          <h2 className="py-2 text-white">Would You Rather Game</h2>
-          <h3>React Js / Redux Toolkit / Semantic UI</h3>
-        </div>
-      </div>
-
-      <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
-        <div className="col-span-4 md:col-span-3 lg:col-span-4">
-          <p>Project</p>
-          <h2 className="py-2">Overview</h2>
-          <p>
-            This is a version of the would you rather game. Users can ask questions and answer any question. For each question asked or answered, a score of 1 is given to the user. To see the score of all the users, you can click on the Leaderboard. <br />
-            This was built with react and state management with redux toolkit. Through this app, I gained useful experience on redux State management.
-          </p>
-          <div className="flex justify-start items-center gap-3">
-            <button className="px-8 py-2 mt-4 mr-8">
-              <Link
-                href="https://chinonso-would-you-rather-game.netlify.app"
-                target="_blank"
-                className="flex justify-between items-center gap-3"
-              >
-                Demo
-                <FiExternalLink />
-              </Link>
-            </button>
-            <button className="px-8 py-2 mt-4">
-              <Link
-                href="https://github.com/ChinonsoIg/would-you-rather-game-with-redux-toolkit"
-                target="_blank"
-                className="flex justify-between items-center gap-3"
-              >
-                Code
-                <FiExternalLink />
-              </Link>
-            </button>
-          </div>
-        </div>
-
-        <div className="col-span-4 md:col-span-2 lg:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
-          <div className="p-2">
-            <p className="text-center font-bold pb-2">Technologies</p>
-            <div className="grid grid-col-3 md:grid-cols-1">
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                React Js
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                Redux Toolkit
-              </p>
-              <p className="text-gray-600 py-2 flex items-center">
-                <RiRadioButtonFill size={16} className="pr-1" />
-                Semantic UI
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <p className="underline cursor-pointer py-10 w-max">
-          <Link href="/#projects">Back</Link>
-        </p>
-      </div>
-    </div>
+    <SingleProject
+      image={would_you_rather_game}
+      title="Would You Rather Game"
+      details={details}
+      liveURL="https://chinonso-would-you-rather-game.netlify.app"
+      githubURL="https://github.com/ChinonsoIg/would-you-rather-game-with-redux-toolkit"
+      stacks={stacks}
+      technologies={technologies}
+    />
   );
 };
 
